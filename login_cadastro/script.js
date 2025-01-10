1e29cbb40a1bd71ebe3943e7e5455f6276208232
Sara Farias <fariassara012@gmail.com>
2025-01-09 21:51:19 -0300

    Telas de cadastros feitas em uma só

--- login_cadastro/script.js
+++ login_cadastro/script.js
@@ -14,18 +14,47 @@ alternarSenha.addEventListener("click", function() {
    this.innerHTML = tipo === "password" ? '<i class="bi bi-eye"></i>' : '<i class="bi bi-eye-slash"></i>';
 });
 
-
 function preVisualizarImagem(evento) {
     const visualizacaoFoto = document.getElementById('photo-preview');
-    const arquivo = evento.target.files[0];
+    const arquivo = evento.target.files[0];   /// sem funcionar 
 
     if (arquivo) {
         const leitor = new FileReader();
         leitor.onload = function (e) {
-            visualizacaoFoto.innerHTML = `<img src="${e.target.result}" alt="Foto do pet">`;
+            visualizacaoFoto.innerHTML = `<img src="${e.tasrget.result}" alt="Foto do pet">`;
         };
         leitor.readAsDataURL(arquivo);
     } else {
         visualizacaoFoto.innerHTML = `<span>+</span>`;
     }
+
 }
+
+
+
+// Exibir imagem de pré-visualização ao selecionar um arquivo
+document.getElementById('foto').addEventListener('change', function(event) {
+    const file = event.target.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            document.getElementById('preview-imagem').src = e.target.result;
+        };
+        reader.readAsDataURL(file);
+    }
+});
+
+
+
+document.getElementById('formPet').addEventListener('submit', function(event) {
+    event.preventDefault(); 
+    
+    alert('Pet cadastrado com sucesso!');
+});
+ 
+
+
+
+
+
+
